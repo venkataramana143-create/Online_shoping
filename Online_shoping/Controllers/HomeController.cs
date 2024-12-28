16ebc3f900e77ac7ab8d359e18567b162daa721b
ADMIN <ADMIN@HP15-PAVILION>
2024-12-28 12:32:39 +0530

    Add project files.

---
+++ Online_shoping/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_shoping.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string name)
+        {
+           
+            return View();
+        }
+        
+        public ActionResult Male()
+        {
+            return View();
+        }
+        public ActionResult Women()
+        {
+            return View();
+        }
+        public ActionResult Kid()
+        {
+            return View();
+        }
+    }
+}
